d122e7719b3f6c2687ade41d39153a529f85bb6d
kmullin8 <kadenmullin@gmail.com>
2024-09-19 13:35:03 -0600

    implemented getAddPiece

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] pieceArray; //locations of all peices
+
     public ChessBoard() {
-        
+        pieceArray = new ChessPiece[8][8];
     }
 
     /**
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        pieceArray[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return(pieceArray[position.getRow() - 1][position.getColumn() - 1]);
     }
 
     /**
