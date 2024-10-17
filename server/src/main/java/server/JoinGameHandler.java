55fc545513c1c5f9d9521709d3ce15a15e6d9826
kmullin8 <kadenmullin@gmail.com>
2024-10-17 13:01:26 -0600

    setup handlers

--- server/src/main/java/server/JoinGameHandler.java
+++ server/src/main/java/server/JoinGameHandler.java
@@ -4,7 +4,7 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-public class ClearHandler implements Route {
+public class JoinGameHandler implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         return null;
