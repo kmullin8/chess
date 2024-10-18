213d378f8bc29a655d0be47c88314338a3838e00
kmullin8 <kadenmullin@gmail.com>
2024-10-18 13:57:42 -0600

    implemented dataAccess and MemoryDataAccess

--- server/src/main/java/dataAccess/DataAccess.java
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -1,4 +1,97 @@
 package dataAccess;
 
-public class DataAccess {
+import model.AuthTokenModel;
+import model.GameModel;
+import model.UserModel;
+
+import java.util.Collection;
+
+/**
+ * Represents all operations that may be performed on the database.
+ */
+public interface DataAccess {
+    /**
+     * Clears out all the data in the database.
+     *
+     * @throws DataAccessException for database or sql query violations (e.g. no error for not found).
+     */
+    void clear() throws DataAccessException;
+
+    /**
+     * Persist a user
+     *
+     * @param user with both username and ID provided.
+     * @throws DataAccessException for database or sql query violations.
+     */
+    UserModel writeUser(UserModel user) throws DataAccessException;
+
+    /**
+     * Read a previously persisted user.
+     *
+     * @param userName for the desired user.
+     * @return The requested @User
+     * @throws DataAccessException for database or sql query violations (e.g. no error for not found).
+     */
+    UserModel readUser(String userName) throws DataAccessException;
+
+    /**
+     * Persist the authorization token. If a token already exists in the database it is overwritten.
+     *
+     * @param username to persist.
+     * @return The @AuthData for the user.
+     * @throws DataAccessException for database or sql query violations.
+     */
+    AuthTokenModel writeAuth(String username) throws DataAccessException;
+
+    /**
+     * Read a previously persisted authorization token.
+     *
+     * @param authToken for the @AuthData to retrieve.
+     * @return The @AuthToken for the user or Null if it doesn't exist.
+     * @throws DataAccessException for database or sql query violations (e.g. no error for not found).
+     */
+    AuthTokenModel readAuth(String authToken) throws DataAccessException;
+
+    /**
+     * Clears out an authorization token. This call is idempotent.
+     *
+     * @param authToken to delete.
+     * @throws DataAccessException for database or sql query violations.
+     */
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    /**
+     * Creates a new game. A new gameID is assigned to the returned object.
+     *
+     * @param gameName to create
+     * @throws DataAccessException for database or sql query violations.
+     */
+    GameModel newGame(String gameName) throws DataAccessException;
+
+    /**
+     * Update an existing game.
+     *
+     * @param game to update
+     * @throws DataAccessException for database or sql query violations.
+     */
+    void updateGame(GameModel game) throws DataAccessException;
+
+    /**
+     * Read a previously persisted Game.
+     *
+     * @param gameID for the game to read
+     * @return The requested Game or null if not found.
+     * @throws DataAccessException for database or sql query violations (e.g. no error for not found).
+     */
+    GameModel readGame(int gameID) throws DataAccessException;
+
+    /**
+     * The complete list of games. Since we don't delete games this will be the full list unless the clear operation is called.
+     *
+     * @return the list of @Game objects
+     * @throws DataAccessException for database or sql query violations.
+     */
+    Collection<GameModel> listGames() throws DataAccessException;
+
+    String description();
 }
--- server/src/main/java/dataAccess/MemoryDataAccess.java
+++ server/src/main/java/dataAccess/MemoryDataAccess.java
@@ -1,4 +1,83 @@
 package dataAccess;
 
-public class MemoryDataAccess {
+import chess.ChessGame;
+import chess.ChessGame;
+import model.AuthTokenModel;
+import model.GameModel;
+import model.UserModel;
+
+import java.util.*;
+
+
+/**
+ * In memory representation of @DataAccess.
+ */
+public class MemoryDataAccess implements DataAccess {
+
+    private int nextID = 1000;
+    final private Map<String, UserModel> users = new HashMap<>();
+    final private Map<String, AuthTokenModel> auths = new HashMap<>();
+    final private Map<Integer, GameModel> games = new HashMap<>();
+
+    public MemoryDataAccess() {
+    }
+
+    public void clear() {
+        users.clear();
+        auths.clear();
+        games.clear();
+    }
+
+    public UserModel writeUser(UserModel user) throws DataAccessException {
+        if (users.get(user.getUsername()) == null) {
+            users.put(user.getUsername(), user);
+            return user;
+        }
+
+        throw new DataAccessException("duplicate");
+    }
+
+    public UserModel readUser(String username) {
+        return users.get(username);
+    }
+
+    public AuthTokenModel writeAuth(String username) {
+        var auth = new AuthTokenModel(username);
+        auths.put(auth.getAuthToken(), auth);
+        return auth;
+    }
+
+    public AuthTokenModel readAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) {
+        auths.remove(authToken);
+    }
+
+    public GameModel newGame(String gameName) {
+        var gameID = nextID++;
+        var newGame = new GameModel(gameID, null, null, gameName, new ChessGame());
+        games.put(newGame.getGameID(), newGame);
+        newGame.getGame().getBoard().resetBoard();
+        newGame.getGame().setTeamTurn(ChessGame.TeamColor.WHITE);
+        return newGame;
+    }
+
+    public void updateGame(GameModel game) {
+        games.put(game.getGameID(), game);
+    }
+
+    public GameModel readGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    public Collection<GameModel> listGames() {
+        return games.values();
+    }
+
+
+    public String description() {
+        return "Memory Database";
+    }
 }
